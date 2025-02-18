7058e1493dc13d7f3838d1f8b4fdf64526233281
paulovictorarmando <paulovictorarmando@gmail.com>
2025-02-18 12:29:35 +0100

    Desafio terminado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,28 +1,22 @@
 import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Solicita e lê o número da agência
+        Scanner scanner = new Scanner(System.in); //criar o scanner
         System.out.print("Por favor, digite o número da Agência: ");
         String agencia = scanner.nextLine();
 
-        // Solicita e lê o número da conta
         System.out.print("Por favor, digite o número da Conta: ");
         int conta = scanner.nextInt();
 
-        // Limpa o buffer antes de ler a string (evita problemas com `nextLine` após `nextInt`)
-        scanner.nextLine();
+        scanner.nextLine(); //limpar o baffer depois de ler int
 
-        // Solicita e lê o nome do cliente
         System.out.print("Por favor, digite o seu nome: ");
         String nome = scanner.nextLine();
 
-        // Solicita e lê o saldo
         System.out.print("Por favor, digite o saldo inicial: ");
         double saldo = scanner.nextDouble();
 
-        // Exibe os dados coletados
+        // MOstrar dados
         System.out.println("\nDados da conta cadastrada:");
         System.out.println("Agência: " + agencia);
         System.out.println("Conta: " + conta);
