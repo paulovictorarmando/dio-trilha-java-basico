bf817a7be48e0133147c3f45356cb763c129ba19
paulovictorarmando <paulovictorarmando@gmail.com>
2025-02-18 12:08:58 +0100

    Configuração do scanner

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,10 @@
-
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.println("Olá " + nome+ ", é um prazer ter-te aqui!");
+        scanner.close();
     }
 }
